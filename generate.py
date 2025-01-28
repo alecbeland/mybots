bac3322020f984591f3561eced50b6df500810fe
alecbeland <alec.beland@uvm.edu>
2025-01-27 20:30:01 -0500

    Two Links

--- generate.py
+++ generate.py
@@ -5,8 +5,20 @@ pyrosim.Start_SDF("box.sdf")
 
 # Add a cube "Box" with pos and size
 length = 1
-width = 2
-height = 3
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[length, width, height])
+width = 1
+height = 1
+x = 0
+y = 0.5
+z = 0
+pyrosim.Send_Cube(name="Box", pos=[x,z,y], size=[length, width, height])
+
+# Add a second cube "Box2" with pos and size
+length = 1
+width = 1
+height = 1
+x = 1
+y = 1.5
+z = 0
+pyrosim.Send_Cube(name="Box2", pos=[x,z,y], size=[length, width, height])
 
 pyrosim.End()
