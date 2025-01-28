5dabc3c62faa878b9d81ad1bad777ab185b4ad5b
alecbeland <alec.beland@uvm.edu>
2025-01-27 20:01:13 -0500

    Added Gravity and Floor

--- simulate.py
+++ simulate.py
@@ -1,7 +1,20 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# Add Gravity
+p.setGravity(0,0,-9.8)
+
+# Add Floor
+planeId = p.loadURDF("plane.urdf")
+
+# Simulate "box"
+p.loadSDF("box.sdf")
+
 for i in range(1000):
 	p.stepSimulation()
 	time.sleep(0.0166)
