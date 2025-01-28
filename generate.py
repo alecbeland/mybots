46f13bfe7df9f0b4855257989d9bbe29b74ae8af
alecbeland <alec.beland@uvm.edu>
2025-01-27 19:18:17 -0500

    Add generate.py which creates a cube FIXED

--- generate.py
+++ generate.py
@@ -4,6 +4,6 @@ import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
 
 # Add a cube "Box" with pos and size
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1]
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
 
 pyrosim.End()
